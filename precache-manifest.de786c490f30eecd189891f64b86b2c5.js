3437b4f0010409687669b853813374a969afe168
syeda sultana <syedamahasultana@gmail.com>
2021-04-21 14:52:41 +0100

    Updates

---
+++ precache-manifest.de786c490f30eecd189891f64b86b2c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccb7c36815d9620ccf56c83cb90111c",
+    "url": "/hangman-game/index.html"
+  },
+  {
+    "revision": "fc3a11d02adf715808ac",
+    "url": "/hangman-game/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "15587dc42cd6c655dae7",
+    "url": "/hangman-game/static/css/main.138e909f.chunk.css"
+  },
+  {
+    "revision": "fc3a11d02adf715808ac",
+    "url": "/hangman-game/static/js/2.74b51bd4.chunk.js"
+  },
+  {
+    "revision": "15587dc42cd6c655dae7",
+    "url": "/hangman-game/static/js/main.8e885172.chunk.js"
+  },
+  {
+    "revision": "4d9d21683e01a6a81fde",
+    "url": "/hangman-game/static/js/runtime-main.2dc97f5c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hangman-game/static/media/logo.25bf045c.svg"
+  }
+]);
